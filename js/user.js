0fd15e1eb79cb1e4835072680d63a73b3fc0fc3c
micahcochran <micahcochran@users.noreply.github.com>
2023-07-12 12:24:28 +0000

    deploy: 5fcea26ca8296ca2ad6a9df50fc1480f92cb0b4e

--- js/user.js
+++ js/user.js
@@ -11,10 +11,21 @@ function copyToClipBoard(obj) {
 
 // add copy to clipboard for every element in the clipboard class.
 let clipboardElements = document.getElementsByClassName('clipboard');
+
 for(cb of clipboardElements) {
         // add click to class='clipboard'
         cb.addEventListener("click", 
                             function() { 
                                 copyToClipBoard(cb)
                             });
+}
+
+// Hide elements in the 'nojs' class.
+// 'nojs' is content intended to be displayed when JavaScript is disabled.
+// This was created so that browsers not having javascript enables could still the text of a screencast.
+// This has the added benefit of making the text available for search engines. 
+let nojsElements = document.getElementsByClassName('nojs');
+
+for(hideElement of nojsElements) {
+        hideElement.style.display = 'none';
 }
